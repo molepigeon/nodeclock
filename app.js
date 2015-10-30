03f53f7810c7e21b09ba6a3f7229ef2d9685252c
Michael Hough <molepigeon@gmail.com>
2015-10-30 16:56:29 +0000

    CHange port to 8080

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(app.router);
 app.use(express.errorHandler());
 app.use(express.static(__dirname + '/public')); //setup static public directory
 
-var port = 80;
+var port = 8080;
 // Start server
 app.listen(port);
 console.log('App started on port ' + port);
